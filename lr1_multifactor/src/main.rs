8a60cf2287d45e914f838cdb421eb704f688532f
SpeedCrash100 <SpeedCrash100@yandex.ru>
2022-02-09 07:55:38 +0300

    lr1 main with dumping population to file

--- lr1_multifactor/src/main.rs
+++ lr1_multifactor/src/main.rs
@@ -1,20 +1,24 @@
+use std::{fs::File, io::Write};
+
 use clap::Parser;
 use genetic_algorithm::{
-    multifactor::{GeneticParameters, VectorFitnessEvaluater, VectorGeneticFactory},
-    GeneticFactory, GeneticProcessor,
+    multifactor::{
+        GeneticParameters, VectorChromosome, VectorFitnessEvaluater, VectorGeneticFactory,
+    },
+    FitnessEvaluater, GeneticFactory, GeneticProcessor,
 };
 
 #[derive(Parser)]
 #[clap(name = "LR1")]
 pub struct CLIParameters {
     /// Radius for searching
-    #[clap(long, default_value = "0.5")]
+    #[clap(long, default_value = "3")]
     pub search_radius: f64,
     /// Radius that allows two chromosomes to cross
-    #[clap(long, default_value = "0.25")]
+    #[clap(long, default_value = "1")]
     pub cross_allow_radius: f64,
     /// Max try count for search chromosome in cross_allow_radius
-    #[clap(long, default_value = "5")]
+    #[clap(long, default_value = "10")]
     pub max_cross_choices: usize,
 
     /// Mutation chance
@@ -27,15 +31,21 @@ pub struct CLIParameters {
     /// Population size
     #[clap(long, default_value = "100")]
     pub population_size: usize,
+
+    /// Population dump
+    #[clap(long, default_value = "dump_pop.csv")]
+    pub population_dump: String,
 }
 
 fn main() {
     let cli = CLIParameters::parse();
 
-    let fitness_func: fn(&Vec<f64>) -> f64 = |v| v[0].powi(2) + v[1].powi(2);
+    let fitness_func: fn(&Vec<f64>) -> f64 =
+        |v| (2500.0 - (v[0].powi(2) + v[1] - 11.0).powi(2) - (v[0] + v[1].powi(2) - 7.0).powi(2));
+    let fe = VectorFitnessEvaluater::new(fitness_func);
 
     let genetic_parameters = GeneticParameters {
-        fitness_evaluater: VectorFitnessEvaluater::new(fitness_func),
+        fitness_evaluater: fe.clone(),
 
         search_radius: cli.search_radius,
         cross_allow_radius: cli.cross_allow_radius,
@@ -45,6 +55,8 @@ fn main() {
         min: vec![-6.0, -6.0],
         max: vec![6.0, 6.0],
     };
+
+    let mut population_file = File::create(cli.population_dump).unwrap();
 
     let factory = VectorGeneticFactory::new(genetic_parameters);
 
@@ -57,10 +69,37 @@ fn main() {
     processor = processor.init_population(population);
     for i in 0..cli.iteration_count {
         println!("Iteration is {}", i);
+        let tmp_pop = processor.population();
+        dump_population_to_file(i, &mut population_file, tmp_pop);
 
-        processor = processor.populate().cross().mutate();
+        processor = processor.populate().cross().mutate().reduce();
     }
 
-    let pop = processor.take_population();
-    println!("Pop #1: {:?}", pop[0]);
+    let mut pop = processor.take_population();
+    dump_population_to_file(cli.iteration_count, &mut population_file, &pop);
+
+    pop.sort_unstable_by(|l, r| fe.fitness(r).partial_cmp(&fe.fitness(l)).unwrap());
+    println!("Top 5 chromosomes: ");
+    for i in 0..5 {
+        println!(
+            "#{}: [{:?}] -- {}",
+            i + 1,
+            &pop[i].point,
+            fe.fitness(&pop[i])
+        )
+    }
+}
+
+fn dump_population_to_file(iteration: usize, file: &mut File, population: &Vec<VectorChromosome>) {
+    for ch in population {
+        writeln!(
+            file,
+            "{}, {}, {}, {}",
+            iteration,
+            ch.point[0],
+            ch.point[1],
+            ch.fitness()
+        )
+        .unwrap();
+    }
 }
